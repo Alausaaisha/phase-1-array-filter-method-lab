5bb057e9a1e90122d2f292e4c9ee1a060cfb8b1a
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-19 21:55:48 +0100

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+// const findMatching = (drivers, name) => {
+//     let filteredDrivers = [];
+//     filteredDrivers = drivers.filter(function(driver){
+//         return (driver === name || driver === name.toLowerCase());
+//     });
+//     return filteredDrivers;
+// }
+
+function findMatching(drivers, name) {
+    let matchingDrivers = [];
+    matchingDrivers = drivers.filter(function(driver){
+        return (driver === name || driver === name.toLowerCase());
+    });
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, name) {
+    let fuzzyDriversMatch = [];
+    fuzzyDriversMatch = drivers.filter(function(driver){
+        return (driver[0] === name[0]);
+    });
+    return fuzzyDriversMatch;
+}
+
+function matchName(drivers, name) {
+    let matchedDriverName = [];
+    matchedDriverName = drivers.filter(function(driver){
+        return (driver.name === name);
+    });
+    return matchedDriverName;
+}
